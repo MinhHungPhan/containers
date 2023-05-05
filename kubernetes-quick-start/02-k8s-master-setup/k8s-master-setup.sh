ca70198c1a1559b73a57364c685428a73908d767
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-05 08:02:29 -0700

    added script to setup k8s master node

---
+++ kubernetes-quick-start/02-k8s-master-setup/k8s-master-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run this script as root or using sudo"
+  exit 1
+fi
+
+# Initialize the cluster using the IP range for Flannel
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+# Copy the kubeadm join command
+echo "Make a note of the kubeadm join command displayed above, as it will be required for worker nodes to join the cluster."
+
+# Set up the local kubeconfig for the current user
+USER_HOME=$(eval echo ~${SUDO_USER})
+KUBECONFIG_DIR="${USER_HOME}/.kube"
+
+mkdir -p "${KUBECONFIG_DIR}"
+cp -i /etc/kubernetes/admin.conf "${KUBECONFIG_DIR}/config"
+chown $(id -u ${SUDO_USER}):$(id -g ${SUDO_USER}) "${KUBECONFIG_DIR}/config"
+
+# Deploy Flannel
+sudo -u ${SUDO_USER} kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel-old.yaml
