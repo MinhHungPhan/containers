f422f2bcac415923261d2734f3e03e683170b148
Minh Hung Phan <minhhung.phan2701@gmail.com>
2023-05-05 08:41:36 -0700

    added script to setup k8s worker node

---
+++ kubernetes-quick-start/03-k8s-worker-setup/k8s-worker-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run this script as root or using sudo"
+  exit 1
+fi
+
+# Prompt for the kubeadm join command
+echo "Please enter the kubeadm join command you copied from the master node:"
+read -r KUBEADM_JOIN_CMD
+
+# Run the join command
+${KUBEADM_JOIN_CMD}
+
+echo "Worker node has joined the cluster. Please check the status of the nodes from the master."
